64b3bcb8809dd89d586bf09f64ca8f5a8bf41908
caguedes17 <caroline.ogs27@gmail.com>
2024-05-01 22:57:31 -0300

    correção do erro JS

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 const titulo = document.getElementById('titulo');
 const listaNaoOrdenada = document.querySelector('ul');
 const link = document.getElementById('link');
@@ -9,11 +8,12 @@ link.innerText = 'Link Proz Educação';
 
 
 listaNaoOrdenada.innerHTML = `
-  <li>Conteudo textual 1</li>
-  <li>Conteudo textual 2</li>
-  <li>Coteudo textual 3</li>
+  <li>Conteúdo textual 1</li>
+  <li>Conteúdo textual 2</li>
+  <li>Conteúdo textual 3</li>
 `;
 listaOrdenada.innerHTML = `
-  <li><a href="https://youtube.com/">Link para Site 1</a></li>
-  <li><a href="https://www.alura.com.br/">Link para Site 2</a></li>
-  <li><a href="https://www.impacta.edu.br/">Link para Site 3</a></li>
+  <li><a href="https://youtube.com/">Link para youtube</a></li>
+  <li><a href="https://www.alura.com.br/">Link para Alura</a></li>
+  <li><a href="https://www.impacta.edu.br/">Link para Impacta</a></li>
+`;
